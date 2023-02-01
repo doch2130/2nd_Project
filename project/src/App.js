306a57ffd4be0f1ad99dc6e0c0a0b71682af383e
HyoHyun <doch2130@gmail.com>
2023-02-01 15:35:24 +0900

    feat: 2023-02-01 1533

--- project/src/App.js
+++ project/src/App.js
@@ -56,7 +56,7 @@ function App() {
       <Routes>
         <Route
           path="*"
-          element={isLoding ? <Loding /> : <CombineComponents />}
+          element={!isLoding ? <Loding /> : <CombineComponents />}
         />
       </Routes>
     </>
