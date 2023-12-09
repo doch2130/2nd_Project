fb7b7a3613b34d54962b941ddcdcae0ef497b8fa
HyoHyun_Home <doch2130@gmail.com>
2023-12-10 01:46:53 +0900

    feat: redis from cloud redis to local redis change

--- backend/redis/redis.js
+++ backend/redis/redis.js
@@ -6,8 +6,23 @@ dotenv.config({
   path: './config/.env',
 });
 
+// exports.redisClient = new Redis({
+//   host: process.env.REDIS_HOST,
+//   port: process.env.REDIS_PORT,
+//   password: process.env.REDIS_PASSWORD,
+// });
+
 exports.redisClient = new Redis({
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASSWORD,
+  // password: process.env.REDIS_PASSWORD,
 });
+
+// exports.SaveData = async (redisConfig, data) => {
+//   const redisClient = redis.createClient(redisConfig);
+//   await redisClient.connect();
+//   await redisClient.set(data.account, data.token);
+//   await redisClient.expireAt(data.account, parseInt(+new Date() / 1000) + 86400);
+//   await redisClient.disconnect();
+//   console.log(`${data.account}'s token save in Redis`);
+// }
